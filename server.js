ecd3e14d33d4e76bccf1fdfe72ad1c1fd6d4b92e
会宾郑 <huibinzheng@huibinzengdembp.asiapacific.hpqcorp.net>
2017-02-09 15:29:10 +0800

    add SSL support for mock server

--- server.js
+++ server.js
@@ -3,7 +3,31 @@ var hack = require('./util/hack.js')
 var bodyParser = require('body-parser');
 var authRouter = require('./router/mock_auth');
 var apiRouter = require('./router/mock_api');
+
 var app = express();
+
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+
+var sslConfig = JSON.parse(fs.readFileSync('ssl.json', 'utf-8'));
+var privateKey  = fs.readFileSync(sslConfig.privatePath, 'utf8');
+var certificate = fs.readFileSync(sslConfig.filePath, 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+var PORT = sslConfig.httpPort;
+var SSLPORT = sslConfig.httpsPort;;
+
+httpServer.listen(PORT, function() {
+    console.log('HTTP Server is running on: http://localhost:%s', PORT);
+});
+httpsServer.listen(SSLPORT, function() {
+    console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
+});
+
+
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,6 +35,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //app.use('/login',authRouter);
 app.use(apiRouter);
 
-app.listen(8089, function () {
-  console.log('github mock server is running on port %d', 8089);
-});
+// app.listen(8089, function () {
+//   console.log('github mock server is running on port %d', 8089);
+// });
